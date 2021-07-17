16f9471bc7c7d7eda7af6bf574367fc54beffbc2
GLukeM-H <glukemh@gmail.com>
2021-07-16 19:09:58 -0600

    Removed console log and commented code

--- scripts/promote.js
+++ scripts/promote.js
@@ -7,18 +7,13 @@ chrome.storage.sync.get(["indicator"], (result) => {
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	console.log("received message");
 	if (request.indicator || request.indicator === null) {
 		indicator = request.indicator;
 	}
 });
 
 const promote = () => {
-	// console.log("promoting ", indicator);
 	if (indicator) {
-		// if (document.querySelector(".breakout-room-header")) {
-		// 	document.querySelector(".breakout-room-header").innerHTML = indicator;
-		// }
 		document.querySelector(`[aria-label^="Promote ${indicator}"]`)?.click();
 	}
 
